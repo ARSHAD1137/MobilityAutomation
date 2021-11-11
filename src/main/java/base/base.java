641d84e152f6ba103b11c8bd159ad4ce3b3777ba
Arshad <arshadpinjari666@gmail.com>
2021-11-11 19:49:39 +0530

    Added.timeoutCapability

--- src/main/java/base/base.java
+++ src/main/java/base/base.java
@@ -1,10 +1,8 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.net.MalformedURLException;
@@ -14,7 +12,7 @@ public class base {
     static WebDriver driver;
 
     @BeforeTest
-    public void setup() throws InterruptedException, MalformedURLException {
+    public void setup() throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability("deviceName", "emulator-5554");
@@ -23,16 +21,10 @@ public class base {
         cap.setCapability("appPackage", "com.android.calculator2");
         cap.setCapability("appActivity", "com.android.calculator2.Calculator");
         cap.setCapability("automationName","UiAutomator2");
-        cap.setCapability("uiautomator2ServerLaunchTimeout","100000");
-        cap.setCapability("adbExecTimeout","60000");
+        cap.setCapability("uiautomator2ServerLaunchTimeout",100000);
+        cap.setCapability("adbExecTimeout",90000);
 
         driver = new RemoteWebDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
-
     }
 
-    @AfterTest
-    public void clearScreen()
-    {
-        driver.findElement(By.xpath("//android.widget.Button[@text='CLR']")).click();
-    }
 }
